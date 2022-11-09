10be08adb67fad805be13d3f14fd20d3da1c0270
Cora Amorim <109560393+coramori@users.noreply.github.com>
2022-11-08 21:52:40 -0300

    Initial commit

---
+++ api.js
@@ -0,0 +1,14 @@
+// const cat_btn = document.getElementById('cat_btn');
+// const dog_btn = document.getElementById('dog_btn');
+// const cat_result = document.getElementById('cat_result');
+// const dog_result = document.getElementById('dog_result');
+
+// cat_btn.addEventListener('click', getRandomCat);
+
+// function getRandomCat() {
+//     fetch('https://aws.random.cat/meow%27)
+//         .then(res => res.json())
+//         .then(data => {
+//             cat_result.innerHTML = <img src=${data.file} alt="cat" />
+//         });
+// }
