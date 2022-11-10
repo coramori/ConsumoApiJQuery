a9600291a9430a41fa1ad753bd85800f53956167
Eduardo A Cestari <eduardo.cestari@gmail.com>
2022-11-10 09:00:30 -0300

    enviado para git

---
+++ _js/library.js
@@ -0,0 +1,11 @@
+/* # Functions*/
+const getPagina = (url, target) => {
+    $.ajax({
+        url: `${url}`,
+        dataType: 'html',
+        success: (pagina) => {
+            $(target).html(pagina);
+        }
+    });
+}
+
